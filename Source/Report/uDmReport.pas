20c19a342b658d91674aaa3beda0f32011367946
cledersoncaruaru <81329482+cledersoncaruaru@users.noreply.github.com>
2022-12-23 21:39:53 -0300

    Mostrando Relatorios Avançados e mais Conteudo que vem Por Ai

--- Source/Report/uDmReport.pas
+++ Source/Report/uDmReport.pas
@@ -3,16 +3,21 @@ unit uDmReport;
 interface
 
 uses
-  System.SysUtils, System.Classes, frxClass, frxExportBaseDialog, frxExportPDF;
+  System.SysUtils, System.Classes, frxClass, frxExportBaseDialog, frxExportPDF,
+  frxDBSet;
 
 type
   TDmReport = class(TDataModule)
     Relatorio: TfrxReport;
     frxPDFExport: TfrxPDFExport;
+    DBRelatorio: TfrxDBDataset;
   private
     { Private declarations }
   public
     { Public declarations }
+
+function Get_Report_Unidades(Page: TObject; TipoRelatorio, COD_UNIDADE, COD_STATUS,COD_DAV,COD_CLIENTE: LongInt; var Report: String;Cache: Boolean): Boolean;
+
   end;
 
 var
@@ -20,8 +25,14 @@ var
 
 implementation
 
+uses
+  FireDAC.Comp.Client, ServerController;
+
 {%CLASSGROUP 'Vcl.Controls.TControl'}
 
 {$R *.dfm}
+
+{ TDmReport }
+
 
 end.
